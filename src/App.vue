3acd4ceaa91187ae1ac48649dc37655215a81e24
AliceCaselli <alicecaselli3@gmail.com>
2023-03-25 00:28:02 +0100

    creazione componenti

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script>
+import AppNavbar from "./components/AppNavbar.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppSection from "./components/AppSection.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+
+
 export default {
   data() {
     return {};
   },
 
+  components:
+    AppNavbar,
+  AppHeader,
+  AppSection,
+  AppFooter,
 
 }
 
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    data() {
+        return {};
+    },
+}
+
+</script>
+
+<template>
+    <div>
+        <img src="/img/dc-logo.png" alt="">
+    </div>
+</template>
+
+<style scoped></style>
